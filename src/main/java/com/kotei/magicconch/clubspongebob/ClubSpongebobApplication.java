868e71b000463b034887e6c4758145cfcb7d5b1b
zzyi <1037803024@qq.com>
2020-02-28 11:58:39 +0800

    初始化项目

---
+++ src/main/java/com/kotei/magicconch/clubspongebob/ClubSpongebobApplication.java
@@ -0,0 +1,11 @@
+package com.kotei.magicconch.clubspongebob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubSpongebobApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ClubSpongebobApplication.class,args);
+    }
+}
