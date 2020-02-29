a6b21621cbe6ef377afc42c29c46aa1b40d0b8fc
zzyi <1037803024@qq.com>
2020-02-29 21:59:23 +0800

    实现并不完全的登陆功能

--- src/main/java/com/kotei/magicconch/clubspongebob/controller/LoginController.java
+++ src/main/java/com/kotei/magicconch/clubspongebob/controller/LoginController.java
@@ -1,13 +1,37 @@
 package com.kotei.magicconch.clubspongebob.controller;
 
+import com.kotei.magicconch.clubspongebob.dto.LoginDTO;
+import com.kotei.magicconch.clubspongebob.pojo.Admin;
+import com.kotei.magicconch.clubspongebob.service.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class LoginController {
+
+    @Autowired
+    private LoginService loginService;
+
     @RequestMapping(value = "/login",method = RequestMethod.GET)
     public String loginPage() {
         return "login";
     }
+
+    @RequestMapping(value = "/login",method = RequestMethod.POST)
+    public String login(LoginDTO loginDTO, HttpServletRequest request) {
+        Admin user = loginService.login(loginDTO);
+        request.getSession().setAttribute("user",user);
+        return "redirect:/";
+    }
+
+    @RequestMapping(value = "/logout",method = RequestMethod.GET)
+    public String logout(HttpServletRequest request) {
+        request.getSession().invalidate();
+        return "redirect:/";
+    }
+
 }
---
+++ src/main/java/com/kotei/magicconch/clubspongebob/dto/LoginDTO.java
@@ -0,0 +1,9 @@
+package com.kotei.magicconch.clubspongebob.dto;
+
+import lombok.Data;
+
+@Data
+public class LoginDTO {
+    private String email;
+    private String password;
+}
