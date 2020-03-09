0bb695d3a166631f6bd99771e13eb21ca49deb6c
zzyi <1037803024@qq.com>
2020-03-09 20:21:29 +0800

    不拦截文章页面

--- src/main/java/com/kotei/magicconch/clubspongebob/config/InterceptorConfig.java
+++ src/main/java/com/kotei/magicconch/clubspongebob/config/InterceptorConfig.java
@@ -21,6 +21,7 @@ public class InterceptorConfig implements WebMvcConfigurer {
         String [] excludePathPatterns={
                 "/",
                 "/login",
+                "/article/**",
                 "/register",
                 "/admin/**",
                 "/assets/**"
